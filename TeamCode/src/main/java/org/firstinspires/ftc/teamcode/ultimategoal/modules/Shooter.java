72a5d5ec89ade4d09209aa91b2ccf2193cc53f04
chop0 <chop@quenda.net>
2020-11-17 15:43:04 -0800

    Si l'objectif n'est pas vu par la caméra, visez en utilisant l'odometry translation, added during horrid rebase: If the camera doesn't see it, use odometry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/modules/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/modules/Shooter.java
@@ -116,7 +116,7 @@ public class Shooter extends ModuleCollection implements Module, TelemetryProvid
     public void aimShooter(TowerGoal target, GoalFinder.GoalLocationData loc) {
         double distanceToTargetCenterRobot = distanceToTarget(target);
         double angleOffset = (DISTANCE_TO_ANGLE_OFFSET_SQUARE_TERM * distanceToTargetCenterRobot * distanceToTargetCenterRobot) + (DISTANCE_TO_ANGLE_OFFSET_LINEAR_TERM * distanceToTargetCenterRobot) + DISTANCE_TO_ANGLE_OFFSET_CONSTANT_TERM;
-        turnToGoal(loc, angleOffset);
+        turnToGoal(target, loc, angleOffset);
 
         double distanceToTarget = distanceToTarget(target, angleWrap(headingToTarget(target) + angleOffset));
         distanceSam = distanceToTarget;
@@ -150,9 +150,12 @@ public class Shooter extends ModuleCollection implements Module, TelemetryProvid
         return yaw > 0.1 ? Math.tanh(Math.pow(yaw, 3)) : 0;
     }
 
-    private void turnToGoal(GoalFinder.GoalLocationData loc, double offset) {
+
+    private void turnToGoal(TowerGoal target, GoalFinder.GoalLocationData loc, double offset) {
         if(loc != null)
-            robot.drivetrain.setBrakeHeading(calculateAngleDelta(loc.getYaw()));
+            robot.drivetrain.setBrakeHeading(calculateAngleDelta(loc.getYaw() + offset));
+        else
+            robot.drivetrain.setBrakeHeading(headingToTarget(target) + offset);
     }
 
     /**
