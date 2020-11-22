eadcb91fce6571ca1ba05a3a0cd5b0a30969d335
Alec Petridis <alec@pop-os.localdomain>
2020-11-22 12:40:59 -0800

    Aiming

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/modules/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/modules/Shooter.java
@@ -147,7 +147,7 @@ public class Shooter extends ModuleCollection implements Module, TelemetryProvid
     }
 
     private double calculateAngleDelta(double yaw) {
-        return yaw > 0.1 ? Math.tanh(Math.pow(yaw, 3)) : 0;
+        return yaw > 0.1 ? Math.tanh(Math.tanh(Math.pow(yaw, 3))) : 0;
     }
 
 
