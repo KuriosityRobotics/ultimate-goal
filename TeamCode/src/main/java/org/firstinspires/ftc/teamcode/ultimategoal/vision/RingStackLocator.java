c2c4ccdecea1a70258a50ef8fccf23af6e87ca85
Alec Petridis <alec@pop-os.localdomain>
2020-11-25 18:43:46 -0800

    Ring detection working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/vision/RingStackLocator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/vision/RingStackLocator.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.ultimategoal.vision;
 
-import org.firstinspires.ftc.teamcode.ultimategoal.util.Target;
 import org.opencv.core.*;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.imgproc.Moments;
@@ -11,7 +10,6 @@ import java.util.stream.Collectors;
 import static org.opencv.imgproc.Imgproc.*;
 
 public class RingStackLocator {
-    public static TargetZone targetZone = TargetZone.TARGET_ZONE_UNKNOWN;
 
     static final Scalar RED = new Scalar(0, 0, 255);
     static final Scalar GREEN = new Scalar(0, 255, 0);
@@ -30,15 +28,15 @@ public class RingStackLocator {
      * @param contours
      * @return the index of the specified List which contains the largest contour
      */
-    static int largestContour(final List<MatOfPoint> contours) {
+    static int largestContour(final List<MatOfPoint> contours, double area) {
         if (contours.size() == 0)
             return -1;
 
         double maxVal = 0;
-        int maxValIdx = 0;
+        int maxValIdx = -1;
         for (int contourIdx = 0; contourIdx < contours.size(); contourIdx++) {
             double contourArea = Imgproc.contourArea(contours.get(contourIdx));
-            if (maxVal < contourArea) {
+            if (maxVal < contourArea && contourArea > 0.005 * area) {
                 maxVal = contourArea;
                 maxValIdx = contourIdx;
             }
@@ -49,33 +47,35 @@ public class RingStackLocator {
 
     /**
      * Find the index of the countour of a list which is closest to the specified point. Contours which are smaller than 0.0008*area will be ignored, so we don't get any super tiny values caused by small imperfections
+     *
      * @param contours
-     * @param point - The point we want to be close to
-     * @param area - the total area of the plane where the contours are located.
+     * @param point    - The point we want to be close to
+     * @param area     - the total area of the plane where the contours are located.
      * @return an index of the specified List which contains the contour closest to the specified point
      */
     static int closestContour(final List<MatOfPoint> contours, Point point, double area) {
         double closestDistToPoint = Double.MAX_VALUE;
         int closestIdx = -1;
         int currIdx = 0;
-        for(MatOfPoint contour: contours) {
+        for (MatOfPoint contour : contours) {
             Rect contourBoundingBox = Imgproc.boundingRect(contour);
             Moments moments = Imgproc.moments(contour); // Calculate the "point of mass" of the enclosed area
             double avgX = moments.m10 / moments.m00;
             double avgY = moments.m01 / moments.m00;
             double dist = Math.hypot(avgX - point.x, avgY - point.y);
-            if(dist < closestDistToPoint && contourBoundingBox.area() > 0.0008 * area) {
+            if (dist < closestDistToPoint && contourBoundingBox.area() > 0.0008 * area) {
                 closestIdx = currIdx;
                 closestDistToPoint = dist;
             }
             currIdx++;
-        };
+        }
+        ;
 
         return closestIdx;
     }
 
     private static double getClosestFromRange(final double input) {
-        double[] numbers = new double[] {0, 1, 4};
+        double[] numbers = new double[]{0, 1, 4};
         List<Double> list = Arrays.stream(numbers).boxed().collect(Collectors.toList());
         return list.stream()
                 .min(Comparator.comparingDouble(i -> Math.abs(i - input))).orElse(0d);
@@ -90,6 +90,7 @@ public class RingStackLocator {
     public static TargetZone processFrame(final Mat input) {
         return processFrame(input, false);
     }
+
     /**
      * This function takes an Image in the form of a Mat, and then will calculate how many rings are stacked in it, and use that to find the target zone
      * It also mutates the input image if shouldWriteToImage is true, and draws some text, a bounding box, and the border of the rings
@@ -98,60 +99,57 @@ public class RingStackLocator {
      * @return an enum representing the target zone
      */
     public static TargetZone processFrame(final Mat input, boolean shouldWriteToImage) {
-        Mat input2 = new Mat();
-
-        Imgproc.resize(input, input2, new Size(480, 270));
+        Imgproc.resize(input, input, new Size(480, 270));
 
         final int IMAGE_WIDTH = input.width();
         final int IMAGE_HEIGHT = input.height();
 
-        int numRings = 0; // This is going to be our guess at how many rings there are
+        float numRings = 0; // This is going to be our guess at how many rings there are
 
         Mat inputHSV = new Mat();
         Mat mask = new Mat(); // This mask will reflect whether or not each pixel in it is yellow enough
         Mat hierarchy = new Mat(); // we don't need to use this, but opencv requires it
 
-        cvtColor(input, inputHSV, COLOR_BGR2HSV); // This converts the colourspace from input to HSV, and then puts that into submatHSV
+        cvtColor(input, inputHSV, COLOR_RGB2HSV); // This converts the colourspace from input to HSV, and then puts that into submatHSV
 
         Imgproc.medianBlur(inputHSV, inputHSV, 9);
-        Core.inRange(inputHSV, new Scalar(0, 135, 0), new Scalar(255, 235, 255), mask); // We now take submatHSV, and work out what is within our colour range that's acceptable. For each pixel, if it is within the range, it will be white; otherwise, it will be black
-        Imgproc.morphologyEx(mask, mask, MORPH_CLOSE, Imgproc.getStructuringElement(MORPH_ELLIPSE, new Size(10, 10)));
-        Imgproc.Canny(mask, mask, 10, 10);
+        Core.inRange(inputHSV, new Scalar(10, 100, 15), new Scalar(25, 255, 255), mask); // We now take submatHSV, and work out what is within our colour range that's acceptable. For each pixel, if it is within the range, it will be white; otherwise, it will be black
+        Imgproc.morphologyEx(mask, mask, MORPH_CLOSE, Imgproc.getStructuringElement(MORPH_ELLIPSE, new Size(5, 5)));
+        //      Imgproc.Canny(mask, mask, 10, 10);
 
         List<MatOfPoint> contours = new ArrayList<MatOfPoint>(); // List for storing contours
         findContours(mask, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE); // Find all the contours (edges) on the mask
-        int closestContour = largestContour(contours); // Find the index of the largest contour
+        //    int closestContour = closestContour(contours, new Point(mask.width() / 2f, (mask.height() / 3f)*2), mask.size().area()); // Find the index of the largest contour
+        int closestContour = largestContour(contours, mask.size().area());
 
-        if(closestContour >= 0 && contours.get(closestContour) != null) { // Do we even have contours?
+        if (closestContour >= 0 && contours.get(closestContour) != null) { // Do we even have contours?
             Rect contourBoundingBox = Imgproc.boundingRect(contours.get(closestContour)); // Draw a bounding box around the largest contour
-            if (contourBoundingBox.area() > 0.0008 * input.size().area()) { // Min size of contour relative to area of image. Using area of BB because mat.size.area is slow
-                numRings = (int)getClosestFromRange((20d * contourBoundingBox.height) / (3d * contourBoundingBox.width)); // We're using the ratio between the height and width of the bounding box to determine how many there are. The 20 and 3 can be calculated from the actual dimensions of the rings. Also am keeping it as an int so it rounds automatically toward zero
-                if (shouldWriteToImage) {
-                    Imgproc.drawContours(input, contours, closestContour, GREEN, 1, LINE_8, hierarchy, 0);
-                    Imgproc.rectangle(input, contourBoundingBox, BLUE, 1);
-                }
-            }
+            numRings = (20f * contourBoundingBox.height) / (3f * contourBoundingBox.width); // We're using the ratio between the height and width of the bounding box to determine how many there are. The 20 and 3 can be calculated from the actual dimensions of the rings. Also am keeping it as an int so it rounds automatically toward zero
+
+            Imgproc.rectangle(input, contourBoundingBox, BLUE, 1);
+
         }
         if (shouldWriteToImage) {
+            for (int i = 0; i < contours.size(); i++) {
+                Imgproc.drawContours(input, contours, i, GREEN, 2, LINE_8, hierarchy);
+            }
             Imgproc.putText(input, String.valueOf(numRings), new Point(0.025 * IMAGE_WIDTH, 0.1 * IMAGE_HEIGHT), FONT_HERSHEY_SCRIPT_COMPLEX, 0.0025 * IMAGE_WIDTH, BLUE, 1);
         }
 
         inputHSV.release();
         hierarchy.release();
         mask.release();
-        input2.release();
-        switch (numRings) {
+        //   cvtColor(input, inputHSV, COLOR_HSV2RGB); // This converts the colourspace from input to HSV, and then puts that into submatHSV
+
+
+        switch ((int) getClosestFromRange(numRings)) {
             case 0:
-                targetZone = TargetZone.TARGET_ZONE_A;
                 return TargetZone.TARGET_ZONE_A;
             case 1:
-                targetZone = TargetZone.TARGET_ZONE_B;
                 return TargetZone.TARGET_ZONE_B;
             case 4:
-                targetZone = TargetZone.TARGET_ZONE_C;
                 return TargetZone.TARGET_ZONE_C;
             default:
-                targetZone = TargetZone.TARGET_ZONE_UNKNOWN;
                 return TargetZone.TARGET_ZONE_UNKNOWN;
         }
     }
