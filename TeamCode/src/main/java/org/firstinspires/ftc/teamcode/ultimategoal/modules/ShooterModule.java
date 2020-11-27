a2c4af5147901b0a73abb93f4e535c3ec67cbbe9
Albert Cai <albertcai.paloalto@gmail.com>
2020-11-27 11:06:37 -0800

    ramps crazy fast but unstable after shot - reaches like 1620 on bounce back

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/modules/ShooterModule.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/modules/ShooterModule.java
@@ -50,8 +50,8 @@ public class ShooterModule implements Module, TelemetryProvider {
 
     @Override
     public boolean initCycle() {
-        flyWheel1.setVelocityPIDFCoefficients(4, 0.3, 0, 11.7);
-        flyWheel2.setVelocityPIDFCoefficients(4, 0.3, 0, 11.7);
+        flyWheel1.setVelocityPIDFCoefficients(8, 0.7, 0, 11.7);
+        flyWheel2.setVelocityPIDFCoefficients(8, 0.7, 0, 11.7);
         return true; // No iterative init required
     }
 
