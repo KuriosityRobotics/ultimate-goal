4817642bc477007b1080d5bf194aa63c7d7a3772
unknown <e3lcool@gmail.com>
2021-03-27 21:09:49 -0700

    UG-54 No coast after reaching brake point

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/util/drivetrain/TargetVelocityFunction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/util/drivetrain/TargetVelocityFunction.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode.ultimategoal.util.drivetrain;
 
 public class TargetVelocityFunction {
-    private final double slowRate, coastThreshold, coastVelocity, stopThreshold;
+    final double slowRate, coastThreshold, coastVelocity, stopThreshold;
 
     public TargetVelocityFunction(double slowRate, double coastThreshold, double coastVelocity, double stopThreshold) {
         this.slowRate = slowRate;
@@ -30,5 +30,9 @@ public class TargetVelocityFunction {
         }
 
         return targetTrueVelocity * sign;
+    }
+
+    public double noCoastDesiredVelocity(double distanceToTarget) {
+        return distanceToTarget * slowRate;
     }
 }
