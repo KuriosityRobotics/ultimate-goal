f7db37c7210c91432d3de606d0b966cb14cc63b1
samduong01 <samduong01@gmail.com>
2021-04-02 20:04:07 -0700

    test code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/opmodes/tests/SamPlebTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/opmodes/tests/SamPlebTest.java
@@ -1,21 +1,25 @@
 package org.firstinspires.ftc.teamcode.ultimategoal.opmodes.tests;
 
+import android.os.SystemClock;
+
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 
-@Disabled
+//@Disabled
 @TeleOp
 public class SamPlebTest extends LinearOpMode {
     private DcMotorEx leftFlyWheel;
     private DcMotorEx rightFlyWheel;
+    private Servo indexer;
 
     public double pos = 0.75;
-    double pos2 = 0.8;
-
-    public double flyWheelTargetSpeed = 1550;
+    double pos2 = 0.68;
+    boolean toggle = false;
+    public double flyWheelTargetSpeed = 1700;
 
     @Override
     public void runOpMode() {
@@ -23,27 +27,44 @@ public class SamPlebTest extends LinearOpMode {
         leftFlyWheel = (DcMotorEx) hardwareMap.get("flyWheel1");
         rightFlyWheel = (DcMotorEx) hardwareMap.get("flyWheel2");
         leftFlyWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        indexer = (Servo) hardwareMap.get("indexer");
 //        rightFlyWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftFlyWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightFlyWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 //        rightFlyWheel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        rightFlyWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftFlyWheel.setVelocityPIDFCoefficients(9, 0.4, 0, 11.7);
+        rightFlyWheel.setVelocityPIDFCoefficients(9, 0.4, 0, 11.7);
 
+        rightFlyWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        indexer.setPosition(pos2);
         waitForStart();
 
+        long startTime = 0;
+        long startTime2 = 0;
+
         while (opModeIsActive()) {
+
             flyWheelTargetSpeed += gamepad1.left_stick_y * 10;
 
             leftFlyWheel.setVelocity(flyWheelTargetSpeed);
-            rightFlyWheel.setVelocity(-flyWheelTargetSpeed);
-            pos += gamepad1.right_stick_y * 0.0001;
+            rightFlyWheel.setVelocity(flyWheelTargetSpeed);
+
             //pos2 +=gamepad2.left_stick_y*0.02;
-            if (gamepad2.a) {
-                pos2 = 0.8;
-            } else if (gamepad2.b) {
-                pos2 = 0.98;
+
+            if (SystemClock.elapsedRealtime()-startTime >= 200 && gamepad1.a) {
+                toggle = !toggle;
+                if(toggle) {
+                    pos2 = 0.68;
+                }else{
+                    pos2 = 0.3;
+                }
+                startTime = SystemClock.elapsedRealtime();
+            }else if(!gamepad1.a){
+                pos2 = 0.68;
             }
+
+            indexer.setPosition(pos2);
 
 
             telemetry.addLine("left speed: " + Double.toString(leftFlyWheel.getVelocity()));
