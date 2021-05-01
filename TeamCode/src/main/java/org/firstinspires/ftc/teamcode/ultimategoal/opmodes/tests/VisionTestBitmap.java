4437159fd05907f522d3b852897c41c1a6605f9a
samduong01 <samduong01@gmail.com>
2021-05-01 14:45:10 -0700

    vision tuned for new cam position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/opmodes/tests/VisionTestBitmap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/opmodes/tests/VisionTestBitmap.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.ultimategoal.vision.Vision;
 
-@Disabled
+//@Disabled
 @TeleOp
 public class VisionTestBitmap extends LinearOpMode {
 
